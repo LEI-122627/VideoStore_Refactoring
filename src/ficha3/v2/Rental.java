6a8e99c0fb26ac987bea26ba5c733ee6c1f5686b
LETI-122639 <jplms2@iscte-iul.pt>
2025-10-23 15:02:35 +0100

    V3.1

--- src/ficha3/v2/Rental.java
+++ src/ficha3/v2/Rental.java
@@ -1,53 +1,44 @@
 package ficha3.v2;
 
-public class Rental
-{
+public class Rental {
 	private Movie _movie;
-	private int		_daysRented;
+	private int _daysRented;
 
-	public Rental(Movie movie, int daysRented)
-	{
+	public Rental(Movie movie, int daysRented) {
 		_movie = movie;
 		_daysRented = daysRented;
 	}
 
-	public int getDaysRented()
-	{
-		return _daysRented;
-	}
-
-	public Movie getMovie()
-	{
+	public Movie getMovie() {
 		return _movie;
 	}
 
-	// Moved pricing logic here
-	public double getCharge() {
+	public int getDaysRented() {
+		return _daysRented;
+	}
+
+	// Calculates the amount for this rental (matches UML's getAmount)
+	public double getAmount() {
 		double result = 0;
-		switch (_movie.getPriceCode()) {
+		Movie.Code code = _movie.getPriceCode();
+
+		switch (code) {
 			case REGULAR:
 				result += 2;
-				if (_daysRented > 2)
+				if (_daysRented > 2) {
 					result += (_daysRented - 2) * 1.5;
+				}
 				break;
 			case NEW_RELEASE:
 				result += _daysRented * 3;
 				break;
 			case CHILDRENS:
 				result += 1.5;
-				if (_daysRented > 3)
+				if (_daysRented > 3) {
 					result += (_daysRented - 3) * 1.5;
+				}
 				break;
 		}
 		return result;
-	}
-
-	// Moved frequent-renter-points logic here
-	public int getFrequentRenterPoints() {
-		int points = 1;
-		if (_movie.getPriceCode() == Movie.Code.NEW_RELEASE && _daysRented > 1) {
-			points++;
-		}
-		return points;
 	}
 }
